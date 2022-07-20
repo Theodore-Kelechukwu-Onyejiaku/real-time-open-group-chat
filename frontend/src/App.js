b8ae51620357f4e06aff84506a40f283d07293df
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2022-07-20 13:43:31 +0100

    add comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import { io } from "socket.io-client";
 import "./App.css"
 
 function App() {
+  // set up toast as ref
   const toastRef = useRef(null)
 
+  // create states
   const [activeUsers, setActiveUsers] = useState(0)
   const [socket, setSocket] = useState(null)
   const [id, setId] = useState("")
@@ -15,12 +17,14 @@ function App() {
   const [notification, setNotification] = useState("");
   const [someoneTyping, setSomeoneTyping] = useState("");
 
+  // scroll to bottom page function when message is sent
   const scrollToBottom = () => {
     window.scroll({
       top: document.body.offsetHeight, left: 0, behavior: 'smooth'
     });
   };
 
+  // function to show toast
   const showToast = (bgColor) => {
     toastRef.current.className = "show"
     toastRef.current.style.backgroundColor = bgColor
@@ -29,6 +33,7 @@ function App() {
     }, 3000)
   }
 
+  // function to change username
   const changeUsername = () => {
     if (!username) return
     setId(username)
@@ -39,10 +44,12 @@ function App() {
     socket.emit("usernameChange", username, socketid)
   }
 
+  // function handle chat message input
   const handleChatInput = (e) => {
     setChat(e.target.value)
   }
 
+  // function to submit chat
   const submitChat = () => {
     if (socket.disconnected) {
       setNotification("You are disconnected. Refresh page to continue")
@@ -55,6 +62,7 @@ function App() {
     scrollToBottom()
   }
 
+  // function to disconnect socket
   const logOut = () => {
     if (socket.disconnected) {
       setNotification("You are already disconnected!")
@@ -66,12 +74,14 @@ function App() {
     }
   }
 
+  // function to get time when message or chat was sent
   const getTime = () => {
     let time = new Date(); let hour = time.getHours(); let minutes = time.getMinutes();
     let meridiem = `${time.getHours() > 12 ? "PM" : "AM"}`;
     return `${hour}:${minutes} ${meridiem}`
   }
 
+  // function to send user
   const searchUser = (socketid) => {
     for (var i = 0; i < allChats.length; i++) {
       if (allChats[i].socketid === socketid) {
@@ -80,35 +90,49 @@ function App() {
     }
   }
 
+  // function to handle when a user or socket is typing
   const handleUserTyping = (e) => {
     clearTimeout(timeout);
     socket.emit("userTyping", socketid, "start")
     var timeout = setTimeout(function () { socket.emit("userTyping", socketid, "stop") }, 1000)
   }
 
+  /* 
+    First useEffect: runs only once and 
+    attaches the events below to a connected socket instance
+  */
   useEffect(() => {
+    // connect with server
     const newSocket = io(process.env.REACT_APP_SERVER_ENDPOINT);
     setSocket(newSocket);
 
+    // if connection error 
     newSocket.on('connect_error', function () {
       alert("Sorry, there seems to be an issue with the connection! Try refreshing");
     })
 
+    // get all users or sockets connected
     newSocket.emit("activeUsers")
+
+    // gets socket id of a socket or user
     newSocket.on("getId", (id) => {
       setId(id)
       setSocketId(id)
     })
 
+    // gets the number of connects 
     newSocket.on("countUsers", (onlineUsers) => {
       setActiveUsers(onlineUsers)
     })
 
+    // adds a message to the list of messages or chats 
+    // and scrolls to bottom of page
     newSocket.on("sendChat", (id, chat, socketid) => {
       setAllChats(prevArr => [...prevArr, { chat, id, socketid, time: getTime() }])
       scrollToBottom()
     })
 
+    // restet all chat when a username is changed
     newSocket.on("resetChat", (username, usersocketid) => {
       setAllChats(prev => {
         return prev.map(
@@ -116,26 +140,34 @@ function App() {
       })
     })
 
+    // triggers when there is a notification
     newSocket.on("message", (message) => {
       setNotification(message)
       showToast("green")
     })
 
-    // clean up
+    // clean up to close instance to avoid side effects
     return () => {
       newSocket.close()
     }
   }, [])
 
+  /*
+   Second useEffect: runs when there is a new chat or message
+  */
   useEffect(() => {
+    // gets connecte users
     socket?.emit("activeUsers")
-    socket?.on("notification", (socketid) => {
+
+    // a notification for disconnection
+    socket?.on("disconnectNotification", (socketid) => {
       let disconnectedUser = searchUser(socketid);
       if (!disconnectedUser || allChats.length === 0) return
       setNotification(disconnectedUser.id + " is disconnected!")
       showToast("#900")
     })
 
+    // event for when a socket or user is typing
     socket?.on("someoneTyping", (socketid, type) => {
       if (type === "stop") {
         setSomeoneTyping("")
@@ -148,6 +180,7 @@ function App() {
 
   return (
     <>
+      {/* HEADER */}
       <div className="fixed p-3 w-full text-center bg-white"><span>Total connected devices:<span className="text-center ml-1 text-base text-green-600">{activeUsers}</span></span><button onClick={logOut} className="ml-5 p-3 shadow-lg text-red-500 rounded bg-gray-100">Logout</button>
         <div>
           <h4 className="text-center">Username:<span className="text-green-600 font-extrabold rounded-full">{id} </span></h4>
@@ -161,7 +194,11 @@ function App() {
         </div>
       </div>
       <div className="">
+        
+        {/* TOAST NOTIFICATION */}
         <p id="toast" ref={toastRef}>{notification}</p>
+
+        {/* MESSAGES OR CHATS */}
         <div className="p-4 md:mx-20 lg:mx-40 mb-40">
           <div className="chat-box mt-40">
             {allChats.map((chat, index) => (
@@ -172,15 +209,16 @@ function App() {
               </div>
             ))}
           </div>
-
         </div>
+
+        {/* KEYBOARD AREA */}
         <div className="fixed z-50 w-full bottom-0 bg-white p-2">
           {someoneTyping && <p className="text-center">{someoneTyping} is typing...</p>}
           <form className="flex flex-row justify-center md:w-1/2 md:m-auto" onSubmit={(e) => { e.preventDefault(); submitChat(chat) }}>
             <input value={chat} onKeyUp={() => { setSomeoneTyping(null) }} onKeyDown={handleUserTyping} onChange={(e) => { handleChatInput(e) }} onKeyPress={(e) => { e.key === "Enter" && handleChatInput(e) }} className="border md:w-3/4 p-4 focus:outline-green-500" placeholder="Type in your message" />
             <button className="p-3 shadow-lg bg-gray-100 rounded text-green-500" type="submit">Send</button>
           </form>
-          <p className="text-center">Made with Love by <a href="https://www.linkedin.com/in/theodore20151014166/" className="underline">Theodore</a></p>
+          <p className="text-center">Made with <span className="text-red-400">&#9829;</span> by <a href="https://www.linkedin.com/in/theodore20151014166/" className="underline">Theodore</a></p>
         </div>
       </div>
     </>
--- server/app.js
+++ server/app.js
@@ -1,35 +1,53 @@
-var express = require("express");
-var cors = require("cors");
-const { application } = require("express");
-var app = express();
+var app = require("express")();
 const server = require("http").createServer(app);
-app.use(cors());
+var cors = require("cors");
+
+// configure socket.io to allow cross-origin requests 
 const io = require("socket.io")(server, { cors: { origin: "*" } });
+
+// set up our PORT 
 const PORT = process.env.PORT || 8000;
 
+// allow cors
+app.use(cors());
+
+// on socket connection
 io.on("connection", (socket) => {
+    // get active or connected socket
     socket.emit("activeUsers")
+
+    // get socket id of connected socket
     socket.emit("getId", socket.id)
+
+    // when a chat event is fired
     socket.on("chat", (id, chat) => {
         io.emit("sendChat", id, chat, socket.id);
     });
-    socket.on("disconnect", () => {
-        socket.broadcast.emit("notification", socket.id);
-        let onlineUsers = io.engine.clientsCount
-        io.emit("countUsers", onlineUsers)
-    });
+
+    // when a user changes their username
     socket.on("usernameChange", (username, socketid) => {
         socket.broadcast.emit("resetChat", username, socketid);
     });
+
+    // when a user is Typing
     socket.on("userTyping", (socketid, type) => {
         socket.broadcast.emit("someoneTyping", socketid, type)
     })
+
+    // get all connected sockets
     socket.on("activeUsers", () => {
+        // gets all connected sockets
         let onlineUsers = io.engine.clientsCount
         io.emit("countUsers", onlineUsers)
     })
+    
+     // when a socket gets disconnected
+     socket.on("disconnect", () => {
+        socket.broadcast.emit("disconnectNotification", socket.id);
+        let onlineUsers = io.engine.clientsCount
+        io.emit("countUsers", onlineUsers)
+    });
 });
-
 
 server.listen(PORT, () => {
     console.log("Application running successfully on port: " + PORT);
